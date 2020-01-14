b8ec8b3998e500a59526187909254b3a3ab798a7
Felipe Armoni <felipe.armoni@gmail.com>
2020-01-14 14:10:45 -0300

    Added Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Box, Button, createStyles, makeStyles, Theme } from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      margin: theme.spacing(2),
+    },
+  })
+);
 
 const App: React.FC = () => {
+  const classes = useStyles();
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className={classes.root}>
+      <Button variant='contained' color='primary'>
+        Hello World!
+      </Button>
+    </Box>
   );
 };
 
