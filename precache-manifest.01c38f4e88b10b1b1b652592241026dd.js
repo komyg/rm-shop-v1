8c82f4a549d1852cfac06f733ae6570ceed7d36f
Felipe Armoni <felipe.armoni@gmail.com>
2020-02-26 09:10:30 -0300

    Updates

---
+++ precache-manifest.01c38f4e88b10b1b1b652592241026dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8408cf56a722f1eaa930588a8c4cb108",
+    "url": "/rm-shop-v1/index.html"
+  },
+  {
+    "revision": "8bbd5636615e8ed4fc12",
+    "url": "/rm-shop-v1/static/js/2.96893703.chunk.js"
+  },
+  {
+    "revision": "8ae279e0396a82bcb324f2e1f01716e9",
+    "url": "/rm-shop-v1/static/js/2.96893703.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "779e6508e60fb4688480",
+    "url": "/rm-shop-v1/static/js/main.123bf0e5.chunk.js"
+  },
+  {
+    "revision": "376d493d40c7991ea38d",
+    "url": "/rm-shop-v1/static/js/runtime-main.a741168b.js"
+  }
+]);
